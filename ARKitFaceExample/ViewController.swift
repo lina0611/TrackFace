d7f7ea5a5219d13bf5c07bff1fed1f70e023f177
lina gao <linagao0611@gmail.com>
2023-05-03 11:05:34 -0400

    initial commit

---
+++ ARKitFaceExample/ViewController.swift
@@ -0,0 +1,227 @@
+/*
+See LICENSE folder for this sample’s licensing information.
+
+Abstract:
+Main view controller for the AR experience.
+*/
+
+import ARKit
+import SceneKit
+import UIKit
+
+class ViewController: UIViewController, ARSessionDelegate {
+    
+    // MARK: Outlets
+
+    @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var tabBar: UITabBar!
+
+    let hatOptions = ["🎓", "🧢", "🎩", "👒", "⛑", " "]
+
+    // MARK: Properties
+    
+    var faceAnchorsAndContentControllers: [ARFaceAnchor: VirtualContentController] = [:]
+    
+    var selectedVirtualContent: VirtualContentType! {
+        didSet {
+            guard oldValue != nil, oldValue != selectedVirtualContent
+                else { return }
+            
+            // Remove existing content when switching types.
+            for contentController in faceAnchorsAndContentControllers.values {
+                contentController.contentNode?.removeFromParentNode()
+            }
+            
+            // If there are anchors already (switching content), create new controllers and generate updated content.
+            // Otherwise, the content controller will place it in `renderer(_:didAdd:for:)`.
+            for anchor in faceAnchorsAndContentControllers.keys {
+                let contentController = selectedVirtualContent.makeController()
+                if let node = sceneView.node(for: anchor),
+                let contentNode = contentController.renderer(sceneView, nodeFor: anchor) {
+                    node.addChildNode(contentNode)
+                    faceAnchorsAndContentControllers[anchor] = contentController
+                }
+            }
+        }
+    }
+    
+    // MARK: - View Controller Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.automaticallyUpdatesLighting = true
+        
+        // Set the initial face content.
+        tabBar.selectedItem = tabBar.items!.first!
+        selectedVirtualContent = VirtualContentType(rawValue: tabBar.selectedItem!.tag)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // AR experiences typically involve moving the device without
+        // touch input for some time, so prevent auto screen dimming.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // "Reset" to run the AR session for the first time.
+        resetTracking()
+    }
+
+    func updateFeatures(for node: SCNNode, using anchor: ARFaceAnchor) {
+      // Search node for a child whose name is “nose” and is of type EmojiNode
+      let child = node.childNode(withName: "hat", recursively: false) as? EmojiNode
+
+      // Get the vertex at index 20 from the ARFaceGeometry property of the ARFaceAnchor and put it into an array.
+      let vertices = [anchor.geometry.vertices[20]]
+
+      child?.updatePosition(for: vertices)
+    }
+
+    // MARK: - ARSessionDelegate
+
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        guard error is ARError else { return }
+        
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
+    }
+    
+    /// - Tag: ARFaceTrackingSetup
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+
+        // Lina: Create a configuration to track a face
+        let configuration = ARFaceTrackingConfiguration()
+        if #available(iOS 13.0, *) {
+            configuration.maximumNumberOfTrackedFaces = ARFaceTrackingConfiguration.supportedNumberOfTrackedFaces
+        }
+        configuration.isLightEstimationEnabled = true
+
+        // Run the face tracking configuration using the built in ARSession property of your AESCNView
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+        
+        faceAnchorsAndContentControllers.removeAll()
+    }
+    
+    // MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // Auto-hide the home indicator to maximize immersion in AR experiences.
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return true
+    }
+    
+    // Hide the status bar to maximize immersion in AR experiences.
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
+
+extension ViewController: UITabBarDelegate {
+    func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let contentType = VirtualContentType(rawValue: item.tag)
+            else { fatalError("unexpected virtual content tag") }
+        selectedVirtualContent = contentType
+    }
+}
+
+extension ViewController: ARSCNViewDelegate {
+
+    // Add a mesh mask
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+          let device = sceneView.device else {
+          return nil
+        }
+
+        let faceGeometry = ARSCNFaceGeometry(device: device)
+        let node = SCNNode(geometry: faceGeometry)
+        node.geometry?.firstMaterial?.fillMode = .lines
+
+        // Hide the mesh mask by making it transparent.
+        node.geometry?.firstMaterial?.transparency = 0.0
+
+        // Create an EmojiNode using your defined nose options.
+        let hatNode = EmojiNode(with: hatOptions)
+
+        // Name the hat node, so it can be found later.
+        hatNode.name = "hat"
+
+        // Add the nose node to the face node.
+        node.addChildNode(hatNode)
+
+        // Call your helper function that repositions facial features.
+        updateFeatures(for: node, using: faceAnchor)
+
+        return node
+      }
+
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        // If this is the first time with this anchor, get the controller to create content.
+        // Otherwise (switching content), will change content when setting `selectedVirtualContent`.
+        DispatchQueue.main.async {
+            let contentController = self.selectedVirtualContent.makeController()
+            if node.childNodes.isEmpty, let contentNode = contentController.renderer(renderer, nodeFor: faceAnchor) {
+                node.addChildNode(contentNode)
+                self.faceAnchorsAndContentControllers[faceAnchor] = contentController
+            }
+        }
+    }
+    
+    /// - Tag: ARFaceGeometryUpdate
+//    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+//        guard let faceAnchor = anchor as? ARFaceAnchor,
+//            let contentController = faceAnchorsAndContentControllers[faceAnchor],
+//            let contentNode = contentController.contentNode else {
+//            return
+//        }
+//
+//        contentController.renderer(renderer, didUpdate: contentNode, for: anchor)
+//    }
+
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+
+        // Ensure the anchor being updated is an ARFaceAnchor and that the node’s geometry is an ARSCNFaceGeometry.
+        guard let faceAnchor = anchor as? ARFaceAnchor,
+        let faceGeometry = node.geometry as? ARSCNFaceGeometry else {
+            return
+        }
+
+        // Update the ARSCNFaceGeometry using the ARFaceAnchor’s ARFaceGeometry
+        faceGeometry.update(from: faceAnchor.geometry)
+
+        updateFeatures(for: node, using: faceAnchor)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        
+        faceAnchorsAndContentControllers[faceAnchor] = nil
+    }
+}
+
